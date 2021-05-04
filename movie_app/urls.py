e78dedd0085264cbba32d06ae72e6c871627a372
BiRuVN <ngngquang99@gmail.com>
2021-05-04 21:04:06 +0700

    add path gerne, movie_genre

--- movie_app/urls.py
+++ movie_app/urls.py
@@ -8,4 +8,9 @@ urlpatterns = [
     path('movie/new', csrf_exempt(add_movie)),
     path('movie/del', csrf_exempt(del_movie)),
     path('movie/update', csrf_exempt(update_movie)),
+
+    path('gerne/', get_gerne),
+    path('gerne/update', csrf_exempt(update_gerne)),
+    path('movie_gerne/add', csrf_exempt(create_movie_gerne))
+    
 ]
