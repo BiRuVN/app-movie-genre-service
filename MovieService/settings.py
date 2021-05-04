f682e509ee274301d9352a71a526aa95ce8a75d4
BiRuVN <ngngquang99@gmail.com>
2021-05-04 22:11:29 +0700

    add cors

--- MovieService/settings.py
+++ MovieService/settings.py
@@ -44,6 +44,9 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'MovieService.middleware.open_access_middleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -51,9 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'MovieService.middleware.open_access_middleware',
+    
 ]
 
 ROOT_URLCONF = 'MovieService.urls'
