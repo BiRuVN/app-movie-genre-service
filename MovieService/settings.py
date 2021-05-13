9d33539251f71535ca3de2f5304e5375b85e6466
BiRuVN <ngngquang99@gmail.com>
2021-05-13 12:51:55 +0700

    cors

--- MovieService/settings.py
+++ MovieService/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 import django_heroku
-from corsheaders.defaults import default_headers
+# from corsheaders.defaults import default_headers
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
