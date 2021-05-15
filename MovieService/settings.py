425c232f6e664f04375c636512aaf1ffbf7edbf4
BiRuVN <ngngquang99@gmail.com>
2021-05-15 16:53:23 +0700

    cors

--- MovieService/settings.py
+++ MovieService/settings.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     # 'corsheaders.middleware.CorsMiddleware',
-    'MovieService.middleware.open_access_middleware',
+    # 'MovieService.middleware.open_access_middleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
