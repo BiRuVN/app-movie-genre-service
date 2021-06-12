37d940bc1779fc1c64b801d5abade5c1331ec630
BiRuVN <ngngquang99@gmail.com>
2021-06-12 18:52:20 +0700

    fix sql

--- movie_app/views.py
+++ movie_app/views.py
@@ -75,7 +75,7 @@ def get_movie(request):
                             FULL JOIN movie_app_genre ON movie_app_movie_genre.genre_id_id = movie_app_genre._id) \
                                 FULL JOIN movie_app_movie ON movie_app_movie_genre.movie_id_id = movie_app_movie._id) \
                                     WHERE movie_app_movie._id IS NOT NULL AND movie_app_movie.status != 1 \
-                                        GROUP BY {}, {}, {}, {}, {}'.format("movie_app_movie._id", *fields[1:], *fields[1:-1], "movie_app_movie._id")
+                                        GROUP BY {}, {}, {}, {}, {}, {}'.format("movie_app_movie._id", *fields[1:], *fields[1:-1], "movie_app_movie._id")
             else:
                 fields = ['movie_id', 'movie_name', 'duration', 'poster', 'release_date', 'trailer', 'description', 'status', 'genre_name']
                 statement = 'SELECT {}, {}, {}, {}, {}, {}, {}, {}, array_agg({}) \
@@ -83,7 +83,7 @@ def get_movie(request):
                             FULL JOIN movie_app_genre ON movie_app_movie_genre.genre_id_id = movie_app_genre._id) \
                                 FULL JOIN movie_app_movie ON movie_app_movie_genre.movie_id_id = movie_app_movie._id) \
                                     WHERE movie_app_movie._id={} AND movie_app_movie.status != 1 \
-                                    GROUP BY {}, {}, {}, {}, {}, {}, {}'.format("movie_app_movie._id", *fields[1:], str(_id), *fields[1:-1], "movie_app_movie._id")
+                                    GROUP BY {}, {}, {}, {}, {}, {}, {}, {}'.format("movie_app_movie._id", *fields[1:], str(_id), *fields[1:-1], "movie_app_movie._id")
         
         all_movies = run_sql(statement)
    
